3e3cea515c97cb0c0b905f31d2a42f2b79eb85df
Daniel Balcells <dbalcells@gmail.com>
2024-01-19 11:45:16 -0500

    Remove extractor and storage attributes from IIRWI as they are already attributes of Retriever

--- iirwi.py
+++ iirwi.py
@@ -4,17 +4,16 @@ from retrieval import CroppingImgRetriever
 from plot import MemePlotter
 
 class IIRWI:
-    def __init__(self, extractor, storage):
-        self.extractor = extractor
-        self.storage = storage
-        self.retriever = CroppingImgRetriever(extractor, storage)
+    def __init__(self, retriever):
+        self.retriever = retriever
         self.plotter = MemePlotter()
 
     @classmethod
     def from_filenames(cls, extractor_filename, storage_filename):
         extractor = FeatureExtractor.load(extractor_filename)
         storage = ImgFeatureStorage.load(storage_filename)
-        return cls(extractor, storage)
+        retriever = CroppingImgRetriever(extractor, storage)
+        return cls(retriever)
     
     def process(self, input_img):
         similar_img, crop_coords = self.retriever.process(input_img)
